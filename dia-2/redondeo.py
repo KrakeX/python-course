6fd22991052f7bd1070289654d38a5ee00bf7fa9
KrakeX <duran.pablo.c@gmail.com>
2022-06-07 02:10:16 -0400

    round

---
+++ dia-2/redondeo.py
@@ -0,0 +1,11 @@
+resultado = round(90/7)
+
+print(resultado)
+
+
+valor = 99.6666666
+valor_redondeado = round(valor)
+print(round(valor))
+
+print(f'El valor redondeado es: {valor_redondeado}')
+print(type(valor_redondeado))
