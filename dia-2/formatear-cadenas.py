0f214685bcbb3137eaebab130e8ab2b25690061b
KrakeX <duran.pablo.c@gmail.com>
2022-06-07 02:02:56 -0400

    operations

---
+++ dia-2/formatear-cadenas.py
@@ -0,0 +1,14 @@
+x = 10
+y = 5
+
+print('Mis números son {} y {}'.format(x, y))
+
+print(f'Mis números son: {x} y {y}')
+
+print('La suma de mis números son {} y {} = {}'.format(x, y, x+y))
+
+
+color = 'rojo'
+matricula = 21341
+
+print(f'El vehículo es de color {color} y matrícula {matricula}')
---
+++ dia-2/operadores.py
@@ -0,0 +1,18 @@
+x = 6
+y = 2
+z = 7
+
+print(f'{x} mas {y} es {x+y}')
+print(f'{x} menos {y} es {x-y}')
+print(f'{x} por {y} es {x*y}')
+print(f'{x} dividido {y} es {x/y}')
+
+print(f'{z} dividido al piso {y} es {z//y}')
+
+print(f'{z} módulo de  {y} es {z%y}')
+
+
+print(f'{x} elevado {y} es {x**y}')
+
+print(f'{x} elevado al cubo es {x**3}')
+print(f'La raíz cuadrada de {x} es {x**0.5}')
