6527c65ec4667d1793fdd5b7e72865e8a524a3d5
Pablo Durán Celis <pablo.durancelis@cencosud.cl>
2022-06-08 01:02:09 -0400

    stringProperties

---
+++ dia-3/stringProperties.py
@@ -0,0 +1,23 @@
+# Los strings son inmutables (No se puede cambiar su contenido ni su orden)
+nombre = 'Carina'
+
+n1 = 'Kari'
+n2 = 'na'
+
+print(n1+n2)
+print(nombre)
+
+# Se pueden multiplicar
+resultado = n1*10
+
+print(resultado)
+
+
+poema = """Mil pequeños peces blancos
+como si hirviera
+el color del agua
+"""
+
+print("agua" in poema)
+print("sol" in poema)
+print("sol" not in poema)
